549d9aa99e12e813a7596c8c7ac78eb31e2032c1
vilmar-moreira-souza <vilmarmoreira2010@gmail.com>
2021-01-17 12:38:09 -0300

    format design

--- churras/script/script.js
+++ churras/script/script.js
@@ -1,51 +1,52 @@
+"use strict";
 
-let inputAdultos = document.getElementById("adultos")
-let inputCriancas = document.getElementById("criancas")
-let inputDuracao = document.getElementById("duracao")
-let resultado = document.getElementById("resultado")
-
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+let resultado = document.getElementById("resultado");
 
 function calcular() {
-  console.log("calculando.....")
+  console.log("calculando.....");
 
-  let adultos = inputAdultos.value
-  let criancas = inputCriancas.value
-  let duracao = inputDuracao.value
+  let adultos = inputAdultos.value;
+  let criancas = inputCriancas.value;
+  let duracao = inputDuracao.value;
 
+  let qtdTotalCarne =
+    carnePP(duracao) * adultos + (carnePP(duracao) / 2) * criancas;
+  let qtdTotalCerveja = cervejaPP(duracao) * adultos;
+  let qtdTotalBebidas =
+    bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2) * criancas;
 
-  let qtdTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao)/2 * criancas)
-  let qtdTotalCerveja = cervejaPP(duracao) * adultos
-  let qtdTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao)/2 * criancas)
-
-
-  resultado.innerHTML = `<p> ${qtdTotalCarne / 1000} kg de carne</P>`
-  resultado.innerHTML += `<p>${Math.ceil(qtdTotalCerveja / 355)} latas de cerveja</P>`
-  resultado.innerHTML += ` <p>${Math.ceil(qtdTotalBebidas / 2000)} garrafas de bebidas</P>`
+  resultado.innerHTML = `<p> ${qtdTotalCarne / 1000} kg de carne</P>`;
+  resultado.innerHTML += `<p>${Math.ceil(
+    qtdTotalCerveja / 355
+  )} latas de cerveja</P>`;
+  resultado.innerHTML += ` <p>${Math.ceil(
+    qtdTotalBebidas / 2000
+  )} garrafas de bebidas</P>`;
 }
-
 
 function carnePP(duracao) {
-  if (duracao>=6) {
-    return 650
-  }else {
-    return 400
+  if (duracao >= 6) {
+    return 650;
+  } else {
+    return 400;
   }
 }
-
 
 function cervejaPP(duracao) {
-  if (duracao>=6) {
-    return 2000
-  }else {
-    return 1200
+  if (duracao >= 6) {
+    return 2000;
+  } else {
+    return 1200;
   }
 }
 
-
 function bebidasPP(duracao) {
-  if (duracao>=6) {
-    return 1500
-  }else {
-    return 1000
+  if (duracao >= 6) {
+    return 1500;
+  } else {
+    return 1000;
   }
 }
